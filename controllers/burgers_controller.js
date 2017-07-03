4e3d1296a06ee217cb46cd40aaaff2f75b212510
Nicholas A Ciccarino <nciccarino@gmail.com>
2017-07-03 16:08:33 -0400

    handlebars

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,7 +5,7 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 router.get("/", function(req, res) {
-  burger.all(function(data) {
+  burger.selectAll(function(data) {
     var hbsObject = {
       burgers: data
     };
@@ -15,7 +15,7 @@ router.get("/", function(req, res) {
 });
 
 router.post("/", function(req, res) {
-  cat.create([
+  burger.insertOne([
     "burger_name", "devoured"
   ], [
     req.body.burger_name, req.body.devoured
@@ -29,7 +29,7 @@ router.put("/:id", function(req, res) {
 
   console.log("condition", condition);
 
-  burger.update({
+  burger.updateOne({
     devoured: req.body.devoured
   }, condition, function() {
     res.redirect("/");
